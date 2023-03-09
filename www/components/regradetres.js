c2bce256d906dbe34b463a46968591fa329151a6
programerkkkkkkkkkkkkkkkkkkk <hipitelo@outlook.com>
2023-03-10 04:35:32 +0900

    Project's Initial Commit

---
+++ www/components/regradetres.js
@@ -0,0 +1,16 @@
+
+
+function b(){
+
+var tres,quatro,cinco,resu;
+
+
+tres= document.getElementById('tres').valor
+quatro= document.getElementById('quatro').valor
+cinco= document.getElementById('cinco').valor
+
+resu= quatro*cinco /tres;
+
+alert(resu);
+
+}
